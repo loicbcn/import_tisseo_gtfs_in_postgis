6e200dcca2571d913573a2f149d6c1beb3a13fd5
loicbcn <loicbcn@users.noreply.github.com>
2019-04-26 11:48:09 +0200

    Contraintes + shapes_geom
    La table shapes_geom contient 1 géométrie par trip_id

--- import.sql
+++ import.sql
@@ -1,24 +1,23 @@
--- Répertoire ou se trouvent les données
-\set datapath 'D:/donnees/tisseo/20190415/'
--- Nom du schéma de la base dans lequel importer les données
-\set schema reseau.
+\set datapath 'D:/donnees/gtfs/20190401_cd31/'
+\set schema reseaubus.
 -- agency.txt
-DROP TABLE IF EXISTS :schema agency;
+DROP TABLE IF EXISTS :schema agency CASCADE;
 CREATE TABLE :schema agency (
-    fid serial PRIMARY KEY,
-    agency_id varchar(20),
+    fid serial NOT NULL,
+    agency_id varchar(20) PRIMARY KEY,
     agency_name varchar(80),
     agency_url varchar(100),
     agency_timezone varchar(20),
+    agency_lang varchar(2),
     agency_phone varchar(20),
-    agency_lang varchar(2)
+    agency_fare_url varchar(100)
 );
 
 -- calendar.txt
-DROP TABLE IF EXISTS :schema calendar;
+DROP TABLE IF EXISTS :schema calendar CASCADE;
 CREATE TABLE :schema calendar (
-    fid serial PRIMARY KEY,
-    service_id varchar(20),
+    fid serial NOT NULL,
+    service_id varchar(20) PRIMARY KEY,
     monday boolean,
     tuesday boolean,
     wednesday boolean,
@@ -31,20 +30,21 @@ CREATE TABLE :schema calendar (
  );
 
 -- calendar_dates.txt
-DROP TABLE IF EXISTS :schema calendar_dates;
+DROP TABLE IF EXISTS :schema calendar_dates CASCADE;
 CREATE TABLE :schema calendar_dates (
-    fid serial PRIMARY KEY,
-    service_id varchar(20),
+    fid serial NOT NULL,
+    service_id varchar(20) REFERENCES :schema calendar (service_id),
     "date" date,
-    exception_type smallint
+    exception_type smallint,
+    CONSTRAINT calendar_dates_pkey PRIMARY KEY(service_id,"date")
 );
 
 -- routes.txt
-DROP TABLE IF EXISTS :schema routes;
+DROP TABLE IF EXISTS :schema routes CASCADE;
 CREATE TABLE :schema routes (
-    fid serial PRIMARY KEY,
-    route_id varchar(20),
-    agency_id varchar(20),
+    fid serial NOT NULL,
+    route_id varchar(20) PRIMARY KEY,
+    agency_id varchar(20) REFERENCES :schema agency(agency_id),
     route_short_name varchar(5),
     route_long_name varchar(55),
     route_desc varchar(80),
@@ -55,58 +55,70 @@ CREATE TABLE :schema routes (
 );
 
 -- shapes.txt
-DROP TABLE IF EXISTS :schema shapes;
+DROP TABLE IF EXISTS :schema shapes CASCADE;
 CREATE TABLE :schema shapes (
-    fid serial PRIMARY KEY,
+    fid serial NOT NULL,
     shape_id varchar(20),
     shape_pt_lat real,
     shape_pt_lon real,
-    shape_pt_sequence smallint
+    shape_pt_sequence smallint,
+    CONSTRAINT shapes_pkey PRIMARY KEY (shape_id, shape_pt_sequence)
 );
 
 -- stop_times.txt
-DROP TABLE IF EXISTS :schema stop_times;
+DROP TABLE IF EXISTS :schema stop_times CASCADE;
 CREATE TABLE :schema stop_times (
-    fid serial PRIMARY KEY,
-    trip_id varchar(20),
-    stop_id varchar(20),
-    stop_sequence smallint,
+    fid serial NOT NULL,
+    trip_id varchar(20) REFERENCES :schema trips (trip_id),
     arrival_time varchar(8), 
     departure_time varchar(8), 
+    stop_id varchar(20) REFERENCES :schema stops (stop_id),
+    stop_sequence smallint,
     stop_headsign varchar(55),
     pickup_type smallint,
     drop_off_type smallint,
-    shape_dist_traveled smallint
+    shape_dist_traveled real,
+    timepoint smallint,
+    CONSTRAINT stop_times_pkey PRIMARY KEY (trip_id, stop_id, stop_sequence)
 );
 
 -- stops.txt
-DROP TABLE IF EXISTS :schema stops;
+DROP TABLE IF EXISTS :schema stops CASCADE;
 CREATE TABLE :schema stops (
-    fid serial PRIMARY KEY,
-    stop_id varchar(20),
+    fid serial NOT NULL,
+    stop_id varchar(20) PRIMARY KEY,
     stop_code varchar(20),
-    stop_name varchar(40),
+    stop_name varchar(80),
+    stop_desc varchar(80),
     stop_lat real,
     stop_lon real,
+    zone_id smallint,
+    stop_url varchar(100),
     location_type smallint,
     parent_station varchar(20),
+    stop_timezone varchar(20),
     wheelchair_boarding smallint
 );
 
 -- trips
-DROP TABLE IF EXISTS :schema trips;
+DROP TABLE IF EXISTS :schema trips CASCADE;
 CREATE TABLE :schema trips (
-    fid serial PRIMARY KEY,
-    trip_id varchar(20),
-    service_id varchar(20),
-    route_id varchar(20), 
+    fid serial NOT NULL,
+    route_id varchar(20) REFERENCES :schema routes (route_id), 
+    service_id varchar(20) REFERENCES :schema calendar (service_id),
+    trip_id varchar(20) PRIMARY KEY,
     trip_headsign varchar(55), 
-    direction_id smallint, 
-    shape_id varchar(20) 
+    trip_short_name varchar(10),
+    direction_id smallint,
+    block_id smallint,
+    shape_id varchar(20),
+    wheelchair_accessible smallint,
+    bikes_allowed smallint    
 );
+
     
 \set path :datapath 'agency.txt'
-COPY :schema agency(agency_id,agency_name,agency_url,agency_timezone,agency_phone,agency_lang) 
+COPY :schema agency(agency_id,agency_name,agency_url,agency_timezone,agency_lang,agency_phone,agency_fare_url) 
 FROM :'path' DELIMITER ',' CSV HEADER ENCODING 'UTF-8';
 
 \set path :datapath 'calendar.txt'
@@ -126,15 +138,15 @@ COPY :schema shapes(shape_id,shape_pt_lat,shape_pt_lon,shape_pt_sequence)
 FROM :'path' DELIMITER ',' CSV HEADER ENCODING 'UTF-8';
 
 \set path :datapath 'stop_times.txt'
-COPY :schema stop_times(trip_id,stop_id,stop_sequence,arrival_time,departure_time,stop_headsign,pickup_type,drop_off_type,shape_dist_traveled) 
+COPY :schema stop_times(trip_id,arrival_time,departure_time,stop_id,stop_sequence,stop_headsign,pickup_type,drop_off_type,shape_dist_traveled,timepoint) 
 FROM :'path' DELIMITER ',' CSV HEADER ENCODING 'UTF-8';
 
 \set path :datapath 'stops.txt'
-COPY :schema stops(stop_id,stop_code,stop_name,stop_lat,stop_lon,location_type,parent_station,wheelchair_boarding ) 
+COPY :schema stops(stop_id,stop_code,stop_name,stop_desc,stop_lat,stop_lon,zone_id,stop_url,location_type,parent_station,stop_timezone,wheelchair_boarding )
 FROM :'path' DELIMITER ',' CSV HEADER ENCODING 'UTF-8';
 
 \set path :datapath 'trips.txt'
-COPY :schema trips(trip_id,service_id,route_id,trip_headsign,direction_id,shape_id ) 
+COPY :schema trips(route_id,service_id,trip_id,trip_headsign,trip_short_name,direction_id,block_id,shape_id,wheelchair_accessible,bikes_allowed ) 
 FROM :'path' DELIMITER ',' CSV HEADER ENCODING 'UTF-8';
 
 
@@ -144,6 +156,26 @@ CREATE INDEX idx_reseau_stops ON :schema stops USING gist (geom);
 
 UPDATE :schema stops 
     SET geom = ST_TRANSFORM(ST_SETSRID(ST_MakePoint(cast(stop_lon as float8), cast(stop_lat as float8)),4326), 2154);
+    
+    
+/* --- Création de shapes_geom -> 1 géométrie / shape_id --- */
+DROP TABLE IF EXISTS :schema shapes_geom CASCADE;
+CREATE TABLE :schema shapes_geom AS (
+	with ordered_shapes as(
+	select st_transform(st_setsrid(st_makepoint(shape_pt_lon, shape_pt_lat),4326), 2154)::geometry(point, 2154) geom, shape_id, shape_pt_lat, shape_pt_lon, shape_pt_sequence 
+	from :schema shapes
+	order by shape_id, shape_pt_sequence)
+
+	select st_makeline(geom)::geometry(linestring, 2154) geom, shape_id 
+	from ordered_shapes 
+	group by shape_id
+);
+
+ALTER TABLE :schema shapes_geom ADD PRIMARY KEY(shape_id);
+CREATE INDEX shapes_geom_gist ON :schema shapes_geom USING GIST (geom);
+
+ALTER TABLE :schema shapes ADD CONSTRAINT fkshapes_shapes_geom FOREIGN KEY (shape_id) REFERENCES :schema shapes_geom (shape_id);
+ALTER TABLE :schema trips ADD CONSTRAINT fktrips_shapes_geom FOREIGN KEY (shape_id)  REFERENCES :schema shapes_geom (shape_id);
     
     
 -- Création d'une table lines contenant chaque route 1 fois (pas de superposition)
